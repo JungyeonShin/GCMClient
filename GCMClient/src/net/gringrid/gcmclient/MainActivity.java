2624a690257e0e6d5f171fe876ca3deee0a62e89
Evangelist <Evangelist@192.168.1.2>
2013-03-25 06:05:23 +0900

    dev ver 1.0

--- GCMClient/src/net/gringrid/gcmclient/MainActivity.java
+++ GCMClient/src/net/gringrid/gcmclient/MainActivity.java
@@ -1,7 +1,12 @@
 package net.gringrid.gcmclient;
 
+import com.google.android.gcm.GCMRegistrar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,7 +15,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		GCMRegistrar.checkDevice(this);
+		GCMRegistrar.checkManifest(this);
+		final String regId = GCMRegistrar.getRegistrationId(this);
+		if ( regId.equals("") ){
+			GCMRegistrar.register(this, "877042154251");
+		}else{
+			Log.d("jiho", "oncreated regId = "+regId);
+			Log.d("jiho", "already registered.");
+		}
+		
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
